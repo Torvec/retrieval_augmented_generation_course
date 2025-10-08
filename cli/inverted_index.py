5102045ccf3cdf2618ced128ff703f7b5150a4da
vonedward <me@edward-vonschondorf.dev>
2025-10-08 08:03:24 -0700

    Add inverted index class

---
+++ cli/inverted_index.py
@@ -0,0 +1,16 @@
+class InvertedIndex:
+    def __init__(self) -> None:
+        self.index = {}
+        self.docmap = {}
+
+    def __add_document(self, doc_id, text):
+        pass
+
+    def get_document(self, term):
+        pass
+
+    def build(self):
+        pass
+
+    def save(self):
+        pass
